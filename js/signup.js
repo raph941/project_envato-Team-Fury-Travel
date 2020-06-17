6785cf2d3bdf134e3b10eaf88b3a9482b65bc276
wendymunyasi <wendymunyasi@gmail.com>
2020-06-17 20:00:49 +0300

    Add form validations to signup.html using js.

--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,9 @@
 (function () {
     'use strict';
     window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('signup__form');
-        // Loop over them and prevent submission
+        // Fetch the forms below we want to apply validation to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over the form and prevent submission
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
